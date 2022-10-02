6940120476af488f404c554a14ee4c5f2a87a5b1
Dave Spina <dspina79@gmail.com>
2022-10-02 12:30:15 -0400

    Project initialization and started arithmetic

---
+++ basic/data-manipulation.exs
@@ -0,0 +1,26 @@
+IO.puts "Getting Started"
+
+# numbers
+IO.puts "Number Initalization"
+x = 10
+y = 5
+
+IO.puts x # 10
+IO.puts y # 5
+
+IO.puts "Arithmetic Operations"
+IO.puts "x + y"
+z = x + y
+IO.puts z
+
+IO.puts "x - y"
+z = x - y
+IO.puts z
+
+IO.puts "x * y"
+z = x * y
+IO.puts z
+
+IO.puts "x / y"
+z = x / y
+IO.puts z
