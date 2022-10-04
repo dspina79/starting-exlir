399eb759114a68da132191d987cacff405bfb525
Dave Spina <dspina79@gmail.com>
2022-10-04 09:15:29 -0400

    Demonstrated string interpolation

--- basic/anonymousfxn/basicgreeting.exs
+++ basic/anonymousfxn/basicgreeting.exs
@@ -1,3 +1,5 @@
 hello = fn name -> "Hello, " <> name <> ". How are you?" end
+goodbye = fn name -> "Goodbye, #{name}" end # string interpolation
 greeting = hello.("Dean")
 IO.puts greeting
+IO.puts goodbye.("Dean")
